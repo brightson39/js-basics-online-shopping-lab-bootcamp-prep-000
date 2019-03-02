a4ec33fbf485b340894546e30a8042979db9e0ef
Bright Olayinka <explorebright@gmail.com>
2019-03-02 18:50:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var itemObj = {itemName: `${item}`, itemPrice: math.floor (math.random() * 100)// write your code here
+ var itemObj = {itemName: `${item}`, itemPrice: math.floor (math.random() * 100)}
+ 
+ // write your code here
 }
 
 function viewCart() {
